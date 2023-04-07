b4f673c01cb07c5cf8db08e771a840d3d587d501
Preston Taylor <95388976+PrestonLTaylor@users.noreply.github.com>
2023-04-07 15:38:38 +0100

    Refactor timestamp endpoints into a seperate class
    Instead of bloating Program.cs the endpoints and the services for it are
    now defined in a seperate class.

---
+++ TimestampMicroservice/Endpoints/TimestampEndpoint.cs
@@ -0,0 +1,18 @@
+﻿namespace TimestampMicroservice.Endpoints
+{
+    public sealed class TimestampEndpoint
+    {
+        static public void DefineEndpoints(WebApplication app)
+        {
+            app.MapGet("/api/", (TimestampService service) => service.GenerateCurrentTimestamp()).WithOpenApi();
+            app.MapGet("/api/{dateString}", (TimestampService service, string dateString) => service.GenerateTimestampFromString(dateString)).WithOpenApi();
+        }
+
+        static public void DefineServices(IServiceCollection services)
+        {
+            // TODO: When .NET 8 comes out we can use the built-in TimeProvider
+            services.AddSingleton<ITimeProvider, TimeProvider>();
+            services.AddSingleton<TimestampService>();
+        }
+    }
+}
--- TimestampMicroservice/Program.cs
+++ TimestampMicroservice/Program.cs
@@ -1,13 +1,11 @@
-using TimestampMicroservice;
+using TimestampMicroservice.Endpoints;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-// TODO: When .NET 8 comes out we can use the built-in TimeProvider
-builder.Services.AddSingleton<ITimeProvider, TimeProvider>();
-builder.Services.AddSingleton<TimestampService>();
+TimestampEndpoint.DefineServices(builder.Services);
 
 var app = builder.Build();
 
@@ -19,7 +17,6 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.MapGet("/api/", (TimestampService service) => service.GenerateCurrentTimestamp()).WithOpenApi();
-app.MapGet("/api/{dateString}", (TimestampService service, string dateString) => service.GenerateTimestampFromString(dateString)).WithOpenApi();
+TimestampEndpoint.DefineEndpoints(app);
 
 app.Run();
