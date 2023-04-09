125037b0626cdbe0a23031ef336a505837b5a214
Preston Taylor <95388976+PrestonLTaylor@users.noreply.github.com>
2023-04-09 14:48:53 +0100

    Use serilog to start logging API requests & responses

---
+++ TimestampMicroservice/Middleware/ResponseLoggingMiddleware.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.HttpLogging;
+
+namespace TimestampMicroservice.Middleware
+{
+    public static class ResponseLoggingMiddlewareExtensions
+    {
+        static public IServiceCollection AddResponseLogging(this IServiceCollection services)
+        {
+            services.AddHttpLogging(logging =>
+            {
+                logging.LoggingFields = HttpLoggingFields.Response;
+            });
+
+            return services;
+        }
+
+        static public WebApplication UseResponseLogging(this WebApplication app)
+        {
+            app.UseWhen(
+                context => context.Request.Path.StartsWithSegments("/api"),
+                builder => builder.UseHttpLogging()
+            );
+
+            return app;
+        }
+    }
+}
--- TimestampMicroservice/Program.cs
+++ TimestampMicroservice/Program.cs
@@ -1,10 +1,18 @@
+using Serilog;
 using TimestampMicroservice.Endpoints;
+using TimestampMicroservice.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddTimestampServices();
+builder.Services.AddResponseLogging();
+
+builder.Host.UseSerilog((context, config) =>
+{
+    config.WriteTo.Console();
+});
 
 var app = builder.Build();
 
@@ -15,6 +23,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseResponseLogging();
 
 app.MapTimestampService();
 
